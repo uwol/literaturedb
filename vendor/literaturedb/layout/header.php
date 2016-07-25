e186408f3adbee72e470676a6b9c50839dbe9d7f
uwol <u.wol@wwu.de>
2016-07-25 12:24:37 +0200

    vendor contents

--- vendor/literaturedb/layout/header.php
+++ vendor/literaturedb/layout/header.php
@@ -11,7 +11,7 @@ if($sessionUser->isLoggedIn()){
 	echo '	<meta name="robots" content="index, noarchive" />'.PHP_EOL;
 }
 ?>
-	<link rel="stylesheet" type="text/css" href="styles/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="vendor/font-awesome/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="custom/styles/screen.css" />
 	<script type="text/javascript" src="styles/jquery.js"></script>
 	<script type="text/javascript" src="styles/tag.js"></script>
